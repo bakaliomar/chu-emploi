528f5298d5f84635b917055ad634fe7d43da1040
bakaliomar <elbakaliomar9@gmail.com>
2023-04-18 16:26:04 +0000

    feat: add add validation and submit candidature form and confirmation

---
+++ src/components/Loader.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.loader
+  .text-center
+    img.commonshare-logo(:src="logo" :class="{ white }")
+  </template>
+
+<script lang="ts" setup>
+import logo from "@/assets/img/loading.gif";
+
+defineProps({
+  white: Boolean,
+});
+</script>
+
+<style lang="scss">
+.loader {
+  font-size: 10px;
+  margin: 25px auto;
+
+  .white {
+    padding: 10px;
+    border-radius: 50%;
+    background-color: #fff;
+  }
+}
+</style>
